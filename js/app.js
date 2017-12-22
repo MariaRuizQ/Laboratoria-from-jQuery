58be8ae019b00809cb74e18fb88e7598defcd3f5
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2017-12-22 13:25:51 -0500

    Añadiendo formulario

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function() {
+  $(function() {
+    var submit = $("#submit");
+    var userName = $("#username");
+    var password = $("#password");
+    var confirmPassword = $("#confirm_password");
+
+    submit.prop('disabled',true);
+
+  });
+});
